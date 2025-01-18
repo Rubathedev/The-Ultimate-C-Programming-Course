e25f7020798737a7a582289f96576c2ff7566f97
dev.Ruabyet <dev.rubayethasan@gmail.com>
2025-01-18 22:29:05 +0600

    Update 08_problem8.c
    1. change the n=1 
    2. add printf and scanf to get the n from the user 
    
    Details: Before it was n=1 which is incorrect the loop should be happen n times not 1 time so i change it , added printf and scanf to store the n's value  . Thank you sir for your videos it helps me a lot

--- Chapter 4 - Practice set/08_problem8.c
+++ Chapter 4 - Practice set/08_problem8.c
@@ -6,11 +6,13 @@ int main(){
     // n! = 1 X 2 X 3 X 4 X 5 ..... X n
     // 0! = 1
     int product=1;
-    int n = 1;
+    int n ;
+    printf("put the number you want to do factorial\n");
+    scanf("%d", &n );
     for (int i = 1; i <= n; i++)
     {
         product *=i;
     }
-    printf("The factorial is %d", product);
+    printf("for %d The factorial is %d\t", n , product);
     return 0;
 }
